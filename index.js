2259ae74b71e1f4f57d116b147a8e6ca02ce51b1
lukas <goncalves.10.lucas@gmail.com>
2019-03-02 19:22:14 -0300

    feat: add seach term and prefix

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ function start(){
     const content = {}
 
     content.searchTerm = askAndReturnSeachTerm()
+    content.prefix = askAndReturnPrefix()
 
     function askAndReturnSeachTerm(){
         return readline.question('Type a Winkipedia seach term: ')
-        
+    }
+    function askAndReturnPrefix(){
+        const prefixes = ['Who is', 'What is', 'The hystory of']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+
+        return selectedPrefixText
     }
 
     console.log(content)
